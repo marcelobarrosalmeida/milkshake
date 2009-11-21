d273c9319e0ac0ef00ac615e1ac726a394067409
marcelobarrosalmeida <marcelobarrosalmeida@4a6f5db0-cb23-11de-a761-efbd646cb129>
2009-11-21 15:29:45 +0000

    new organization and renaming groups to lists

--- trunk/src/default.py
+++ trunk/src/default.py
@@ -1,3 +1,30 @@
+# -*- coding: utf-8 -*-
+######################################################################
+# Milkshake
+# Copyright (C) 2009  Marcelo Barros & Jose Antonio Oliveira
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Milkshake  
+# Copyright (C) 2009  Marcelo Barros & Jose Antonio Oliveira
+# This program comes with ABSOLUTELY NO WARRANTY; for details see 
+# about box.
+# This is free software, and you are welcome to redistribute it
+# under certain conditions; see about box for details.
+######################################################################
+
 import e32
 import sys
 import os
@@ -18,4 +45,3 @@ try:
 except Exception, e:
     import appuifw
     appuifw.app.body = appuifw.Text(repr(e))
-        
