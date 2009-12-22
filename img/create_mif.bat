7476324a880c6db68cbc5c52b096054f9351349c
marcelobarrosalmeida@gmail.com <marcelobarrosalmeida@gmail.com@4a6f5db0-cb23-11de-a761-efbd646cb129>
2009-12-22 02:56:16 +0000

    new icons for task progress

---
+++ img/create_mif.bat
@@ -0,0 +1,5 @@
+.\bin\mifconv.exe milkshake.mif /c32 p00.svg /c32 p10.svg /c32 p20.svg /c32 p30.svg /c32 p40.svg /c32 p50.svg /c32 p60.svg /c32 p70.svg /c32 p80.svg /c32 p90.svg /c32 p100.svg /X /S.\bin\ /Hmilkshake.mbg
+pause
+
+
+
